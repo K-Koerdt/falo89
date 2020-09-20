e4253834c49705dc14b0673b6fb0a89a2674e8e3
navic1992 <civan.erbay@web.de>
2020-09-20 14:24:01 +0200

    Set Up basic HTML

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,7 +4,14 @@ import "./Landing.css"
 export default function Landing() {
     return (
         <div className="container">
-            <h1>Kevin Koerdt</h1>
+            <nav>Navbar</nav>
+            <div className="container__video">Video</div>
+            <div className="container__welcome">Willkommen</div>
+            <div className="container__offer">Angebote</div>
+            <div className="container__references">Referenzen</div>
+            <div className="container__newsletter">Newsletter</div>
+            <div className="container__explore">Entdecken</div>
+            <footer></footer>
         </div>
     )
 }
