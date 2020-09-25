da29fe4593cff7135328df90c332387afae643d3
navic1992 <civan.erbay@web.de>
2020-09-25 16:35:33 +0200

    Add Video

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import "./Landing.scss"
+import React from "react";
+import "./Landing.scss";
 
 export default function Landing() {
-    return (
-        <div className="container">
-            <nav id="nav">Navbar</nav>
-            <div className="container__video">Video</div>
-            <div className="container__welcome">Willkommen</div>
-            <div className="container__offer">Angebote</div>
-            <div className="container__references">Referenzen</div>
-            <div className="container__newsletter">Newsletter</div>
-            <div className="container__explore">Entdecken</div>
-            <footer>Footer</footer>
+  return (
+    <div className="container">
+      <nav id="nav" className="container__nav">
+        <div className="container__nav__div1">Falo89</div>
+        <div className="container__nav__div2"><span>Angebot</span><span>Referenzen</span><span>Entdecken</span></div>
+      </nav>
+      <div className="container__video">
+        <div className="container__video__div">
+          <iframe
+            src="https://www.youtube.com/embed/5qap5aO4i9A"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
         </div>
-    )
+      </div>
+      <div className="container__welcome">Willkommen</div>
+      <div className="container__offer">Angebote</div>
+      <div className="container__references">Referenzen</div>
+      <div className="container__newsletter">Newsletter</div>
+      <div className="container__explore">Entdecken</div>
+      <footer>Footer</footer>
+    </div>
+  );
 }
