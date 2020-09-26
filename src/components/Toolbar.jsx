584974806ba9971e76063b8cdbb0539f5d34c0d3
navic1992 <civan.erbay@web.de>
2020-09-26 16:20:52 +0200

    Add DrawerToggleButton Componenet

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import './Toolbar.scss'
+import './DrawerToggleButton'
+import DrawerToggleButton from "./DrawerToggleButton";
+
 const Toolbar = (props) => (
   <header className="toolbar">
     <nav id="nav" className="toolbar__navigation">
+        <div>
+            <DrawerToggleButton />
+        </div>
       <div className="toolbar__navigation__logo">Logo</div>
       <div className="toolbar__navigation__items">
         <a href="">Angebot</a>
