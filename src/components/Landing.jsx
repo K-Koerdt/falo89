f469e111053bd2d841eafe36c59885e9fcba55e3
navic1992 <civan.erbay@web.de>
2020-09-26 14:57:49 +0200

    Add Landing Loop Video

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,28 +1,37 @@
-import React from "react";
 import "./Landing.scss";
+import React from 'react';
 
 export default function Landing() {
+
+  
+
   return (
     <div className="container">
-      <nav id="nav" className="container__nav">
-        <div className="container__nav__div1">Falo89</div>
-        <div className="container__nav__div2"><span>Angebot</span><span>Referenzen</span><span>Entdecken</span><span>Kontakt</span></div>
-      </nav>
-      <div className="container__video">
-        <div className="container__video__div">
-          <iframe
-            src="https://www.youtube.com/embed/5qap5aO4i9A"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
+   <nav id="nav" className="container__nav">
+        <div className="container__nav__div1">Logo</div>
+        <div className="container__nav__div2">
+          <span>Angebot</span>
+          <span>Referenzen</span>
+          <span>Entdecken</span>
+          <span>Kontakt</span>
         </div>
+      </nav> 
+      <section className="showcase">
+        <div className="video-container">
+          <video src="./video/Underwater.mp4" autoPlay muted loop></video>
+        </div>
+        <div className="content">
+           <h1>Fabian Lorenz</h1>
+        
+        </div>
+      </section>
+      <div className="container__welcome">
+        <h2>Willkommen</h2>
       </div>
-      <div className="container__welcome"><h1>Willkommen</h1></div>
       <div className="container__offer">Angebote</div>
       <div className="container__references">Referenzen</div>
-      <div className="container__newsletter">Newsletter</div>
-      <div className="container__explore">Entdecken</div>
+      <div className="container__newsletter">Newsletter</div>   
+      <div className="container__newsletter">Entdecken</div>
       <footer>Footer</footer>
     </div>
   );
