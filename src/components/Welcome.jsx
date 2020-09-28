f7d55cfcaefaf65b3dc33a409dc54692667188d2
navic1992 <civan.erbay@web.de>
2020-09-28 15:14:33 +0200

    Welcome section Rework

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,7 +5,12 @@ export const Welcome = () => {
     return (
 <>
 <div className="container__welcome">
-   <h1> Herzlich Willkommen </h1>
+        <div className="container__welcome__h1">
+            <h1 style={{paddingTop: "1em"}}>Herzlich</h1>
+            <div style={{display: "flex", justifyContent: "center"}}>
+            <h1 style={{paddingBottom: "1em", color: "orange"}}>Willkommen</h1>
+            </div>
+        </div>
    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
 </div>
 </>
