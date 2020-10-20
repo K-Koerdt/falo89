b41d067373d2b56e1feeb942d8bfb729b5ca8632
Civan <civan.erbay@web.de>
2020-10-20 16:55:26 +0200

    Add Column System for Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import './Footer.scss'
+import "./Footer.scss";
 
 export const Footer = () => {
   return (
     <div className="container__footer" id="footer">
-      <h2>FOOTER</h2>
+      <div className="container__footer__links">
+        <h3 style={{padding: "20px 0"}}> Quick Links</h3>
+        <p> Kontakt </p>
+        <p> Angebote </p>
+        <p>AGB</p>
+        <p>Disclaimer</p>
+        <p>Datenschutz</p>
+        <p>Impressum</p>
+      </div>
     </div>
   );
 };
